1d5d66c5c08582c911c6e855b61552d9254c47a0
Erik HR <writeerikh@gmail.com>
2023-02-13 12:43:23 +0700

    add routes and styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { ReactDOM, createRoot } from "react-dom/client";
 import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
 } from "react-router-dom";
 
 import "./index.css";
 
 import Header from "./shared/Header";
 import About from "./pages/About";
+import ProfileTemplate from "./pages/About/profileTemplate";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Darkroom from "./pages/Darkroom";
@@ -21,34 +22,38 @@ const NavigationContext = React.createContext();
 const location = "/";
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Home />,
-	},
-	{
-		path: "/contact",
-		element: <Contact />,
-	},
-	{
-		path: "/about",
-		element: <About />,
-	},
-	{
-		path: "/darkroom",
-		element: <Darkroom />,
-	},
-	{
-		path: "/gallery",
-		element: <Gallery />,
-	},
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/darkroom",
+    element: <Darkroom />,
+  },
+  {
+    path: "/gallery",
+    element: <Gallery />,
+  },
+  {
+    path: "/pp/temp",
+    element: <ProfileTemplate />,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<NavigationContext.Provider value={location}>
-			<Header>
-				<RouterProvider router={router} />
-			</Header>
-		</NavigationContext.Provider>
-	</React.StrictMode>,
+  <React.StrictMode>
+    <NavigationContext.Provider value={location}>
+      <Header>
+        <RouterProvider router={router} />
+      </Header>
+    </NavigationContext.Provider>
+  </React.StrictMode>
 );
